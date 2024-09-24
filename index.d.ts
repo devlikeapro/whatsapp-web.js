1f85b0340881b4cbe06a75104002c7daa80a1ffd
devlikepro <waha@devlike.pro>
2024-09-24 11:35:25 +0700

    [WAHA] Add Label.colorIndex

--- index.d.ts
+++ index.d.ts
@@ -1356,6 +1356,8 @@ declare namespace WAWebJS {
         id: string,
         /** Color assigned to the label */
         hexColor: string,
+        /** Color index for the label */
+        colorIndex: number,
 
         /** Get all chats that have been assigned this Label */
         getChats: () => Promise<Chat[]>
