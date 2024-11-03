63966eef8382acacb29751180e0e03baac2c1d13
devlikepro <waha@devlike.pro>
2024-11-03 15:49:48 +0700

    [WAHA] Send broadcast status

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -25,6 +25,7 @@ exports.LoadUtils = () => {
 
     window.WWebJS.sendMessage = async (chat, content, options = {}) => {
         const isChannel = window.Store.ChatGetters.getIsNewsletter(chat);
+        const isStatusBroadcast = chat.id._serialized === 'status@broadcast';
 
         let mediaOptions = {};
         if (options.media) {
@@ -226,6 +227,11 @@ exports.LoadUtils = () => {
             from = chat.groupMetadata && chat.groupMetadata.isLidAddressingMode ? lidUser : meUser;
             participant = window.Store.WidFactory.toUserWid(from);
         }
+        
+        if (isStatusBroadcast) {
+            from = meUser;
+            participant = window.Store.WidFactory.toUserWid(from);
+        }
 
         const newMsgKey = new window.Store.MsgKey({
             from: from,
@@ -298,6 +304,18 @@ exports.LoadUtils = () => {
             msg.updateAck(1, true);
             await window.Store.SendChannelMessage.updateNewsletterMsgRecord(msg);
             return msg;
+        }
+        
+        if (isStatusBroadcast) {
+            const isMedia = Object.keys(mediaOptions).length > 0;
+            if (!isMedia) {
+                throw new Error('Status broadcast messages must be media messages');
+            }
+            await window.Store.SendStatus.sendStatusMediaMsgAction(
+                message, 
+                async (x) => x, // accepts "uploadMedia" callback, but we uploaded it before
+            );
+            return window.Store.Msg.get(newMsgKey._serialized);
         }
 
         await window.Store.SendMessage.addAndSendMsgToChat(chat, message);
