bb1f5b69fb65c82a53f2426c88c6a9a070abafcd
devlikepro <waha@devlike.pro>
2024-12-01 16:20:57 +0700

    [WAHA] isChannel
    [WAHA] Fix isChannel
    
    # Conflicts:
    #	src/util/Injected/Utils.js

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -586,6 +586,7 @@ exports.LoadUtils = () => {
                     await window.WWebJS.getChannelMetadata(chatId);
                     chat = await window.Store.WAWebNewsletterMetadataCollection.find(chatWid);
                 }
+                chat.isChannel = true;
             } catch (err) {
                 chat = null;
             }
@@ -662,9 +663,11 @@ exports.LoadUtils = () => {
             model.isReadOnly = chat.groupMetadata.announce;
         }
 
-        if (chat.newsletterMetadata) {
+        isChannel = chat.id.server === 'newsletter';
+        if (isChannel) {
             const newsletterMetadata = window.Store.NewsletterMetadataCollection || window.Store.WAWebNewsletterMetadataCollection;
             await newsletterMetadata.update(chat.id);
+            model.isChannel = true;
             model.channelMetadata = chat.newsletterMetadata.serialize();
             model.channelMetadata.createdAtTs = chat.newsletterMetadata.creationTime;
         }
