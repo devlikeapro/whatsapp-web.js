d436dc8aae3daf904390439dd7c145d148b5cac2
devlikepro <waha@devlike.pro>
2024-12-06 10:06:55 +0700

    [WAHA] Fix get poll from channel

--- src/structures/Message.js
+++ src/structures/Message.js
@@ -294,7 +294,9 @@ class Message extends Base {
             this.allowMultipleAnswers = Boolean(!data.pollSelectableOptionsCount);
             this.pollInvalidated = data.pollInvalidated;
             this.isSentCagPollCreation = data.isSentCagPollCreation;
-            this.messageSecret = data.messageSecret ? Object.keys(data.messageSecret).map((key) => data.messageSecret[key]) : [];
+            if (data.messageSecret) {
+                this.messageSecret = data.messageSecret ? Object.keys(data.messageSecret).map((key) => data.messageSecret[key]) : [];
+            }
         }
 
         return super._patch(data);
