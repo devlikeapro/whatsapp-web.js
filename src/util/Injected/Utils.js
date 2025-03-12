53c12a331bc6add5426b41b309542fccb08f02a3
devlikepro <waha@devlike.pro>
2025-03-12 09:10:50 +0700

    [WAHA] emit newMesageId

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -249,6 +249,9 @@ exports.LoadUtils = () => {
         const lidUser = window.Store.User.getMaybeMeLidUser();
         const meUser = window.Store.User.getMaybeMePnUser();
         const newId = await window.Store.MsgKey.newId();
+        if (typeof window.onNewMessageId === 'function') {
+            window.onNewMessageId(newId);
+        }
         let from = chat.id.isLid() ? lidUser : meUser;
         let participant;
 
