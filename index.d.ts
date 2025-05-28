c25e23c9a64928b99aaa8c843a528ffb315732b9
devlikepro <waha@devlike.pro>
2025-05-28 13:35:03 +0700

    [WAHA] Add tag:chatstate types
    (cherry picked from commit 5b7cd1d930363b3b558ccda96f5b812ae08b80b0)

--- index.d.ts
+++ index.d.ts
@@ -522,6 +522,9 @@ declare namespace WAWebJS {
         /** Emitted when an presence message tag */
         on(event: 'tag:presence', listener: (data: any) => void): this;
 
+        /** Emitted when an chatsate tag */
+        on(event: 'tag:chatstate', listener: (data: any) => void): this;
+
         /** Emitted when an edit event occurrs on message type */
         on(
             event: 'message_edit',
@@ -1031,6 +1034,7 @@ declare namespace WAWebJS {
         MESSAGE_ACK = 'message_ack',
         TAG_RECEIPT = 'tag:receipt',
         TAG_PRESENCE = 'tag:presence',
+        TAG_CHATSTATE = 'tag:chatstate',
         MESSAGE_EDIT = 'message_edit',
         UNREAD_COUNT = 'unread_count',
         MESSAGE_REACTION = 'message_reaction',
