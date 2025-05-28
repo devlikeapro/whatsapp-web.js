a51d6c86ca45437d07d9fb9a7480679da0c8d51e
devlikepro <waha@devlike.pro>
2025-05-28 12:11:12 +0700

    [WAHA] Add tag:presence to events

--- index.d.ts
+++ index.d.ts
@@ -421,6 +421,11 @@ declare namespace WAWebJS {
         on(event: 'tag:receipt', listener: (
             data: any
         ) => void): this
+
+        /** Emitted when an presence message tag */
+        on(event: 'tag:presence', listener: (
+            data: any
+        ) => void): this
         
         /** Emitted when an edit event occurrs on message type */
         on(event: 'message_edit', listener: (
@@ -895,6 +900,7 @@ declare namespace WAWebJS {
         MESSAGE_REVOKED_ME = 'message_revoke_me',
         MESSAGE_ACK = 'message_ack',
         TAG_RECEIPT = 'tag:receipt',
+        TAG_PRESENCE = 'tag:presence',
         MESSAGE_EDIT = 'message_edit',
         UNREAD_COUNT = 'unread_count',
         MESSAGE_REACTION = 'message_reaction',
