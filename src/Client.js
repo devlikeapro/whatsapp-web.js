270dad0498b957e88a60446f0639ed72a2856f72
devlikepro <waha@devlike.pro>
2025-07-02 11:30:05 +0700

    [WAHA] Send onTag without blocking the result

--- src/Client.js
+++ src/Client.js
@@ -737,7 +737,7 @@ class Client extends EventEmitter {
                 window.Store.SocketWap.decodeStanza = async (...args) => {
                     const result = await window.decodeStanzaBack(...args);
                     if (tags.includes(result?.tag)){
-                        window.onTag(result);
+                        setTimeout(() => window.onTag(result), 0);
                     }
                     return result;
                 };
