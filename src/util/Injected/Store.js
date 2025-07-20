3db9c9d867a0cb5be8525552ad2dc7de9ba42580
devlikepro <waha@devlike.pro>
2025-07-20 13:03:54 +0700

    [WAHA] Fix send seen on chat - Stream markAvailable

--- src/util/Injected/Store.js
+++ src/util/Injected/Store.js
@@ -99,6 +99,7 @@ exports.ExposeStore = () => {
     window.Store.AddonReactionTable = window.require('WAWebAddonReactionTableMode').reactionTableMode;
     window.Store.ChatGetters = window.require('WAWebChatGetters');
     window.Store.PinnedMsgUtils = window.require('WAWebSendPinMessageAction');
+    window.Store.WAWebStreamModel = window.require('WAWebStreamModel');
     
     window.Store.Settings = {
         ...window.require('WAWebUserPrefsGeneral'),
