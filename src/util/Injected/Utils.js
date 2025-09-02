d3c1b36c643ea671b64a1d369b6526de7ffa705a
BenyFilho <168232825+BenyFilho@users.noreply.github.com>
2025-09-01 21:56:56 -0300

    fix Event Ready Again and SendMessage (#3747)
    * fix Event Ready Again
    
    * Fixing SendMessage
    
    * Fixing RejectCall
    
    (cherry picked from commit 4ac22c8c3721d82343213574fecd011c9c758826)

--- src/util/Injected/Utils.js
+++ src/util/Injected/Utils.js
@@ -228,7 +228,7 @@ exports.LoadUtils = () => {
         }
 
         const lidUser = window.Store.User.getMaybeMeLidUser();
-        const meUser = window.Store.User.getMaybeMeUser();
+        const meUser = window.Store.User.getMaybeMePnUser();
         const newId = await window.Store.MsgKey.newId();
         if (typeof window.onNewMessageId === 'function') {
             window.onNewMessageId(newId);
@@ -826,7 +826,7 @@ exports.LoadUtils = () => {
 
     window.WWebJS.rejectCall = async (peerJid, id) => {
         peerJid = peerJid.split('@')[0] + '@s.whatsapp.net';
-        let userId = window.Store.User.getMaybeMeUser().user + '@s.whatsapp.net';
+        let userId = window.Store.User.getMaybeMePnUser().user + '@s.whatsapp.net';
         const stanza = window.Store.SocketWap.wap('call', {
             id: window.Store.SocketWap.generateId(),
             from: window.Store.SocketWap.USER_JID(userId),
