2212da6bb45b6d047765578b9b9bfc4c3ee48d4f
devlikepro <waha@devlike.pro>
2025-11-24 17:26:18 +0700

    [WAHA] Fix code pairing on demand
    (cherry picked from commit ade35a66399347aef473fc189eeea49a540f8e51)

--- src/Client.js
+++ src/Client.js
@@ -180,6 +180,21 @@ class Client extends EventEmitter {
                 }
 
                 // Register qr/code events
+                await exposeFunctionIfAbsent(
+                    this.pupPage,
+                    'onCodeReceivedEvent',
+                    async (code) => {
+                        /**
+                         * Emitted when a pairing code is received
+                         * @event Client#code
+                         * @param {string} code Code
+                         * @returns {string} Code that was just received
+                         */
+                        this.emit(Events.CODE_RECEIVED, code);
+                        return code;
+                    },
+                );
+
                 if (pairWithPhoneNumber.phoneNumber) {
                     this.requestPairingCode(
                         pairWithPhoneNumber.phoneNumber,
