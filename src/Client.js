3b8999cec79a37bf4d3aea62988db175f73eb19f
devlikepro <waha@devlike.pro>
2026-03-12 10:25:54 +0700

    [WAHA] Fix getProfilePicUrl - use id: chatWid in requestProfilePicFromServer

--- src/Client.js
+++ src/Client.js
@@ -1528,7 +1528,7 @@ class Client extends EventEmitter {
         const profilePic = await this.pupPage.evaluate(async contactId => {
             try {
                 const chatWid = window.Store.WidFactory.createWid(contactId);
-                return await window.Store.ProfilePic.requestProfilePicFromServer(chatWid);
+                return await window.Store.ProfilePic.requestProfilePicFromServer({id: chatWid});
             } catch (err) {
                 if(err.name === 'ServerStatusCodeError') return undefined;
                 throw err;
